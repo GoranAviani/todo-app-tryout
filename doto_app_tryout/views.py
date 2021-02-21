df9fc81166c826a0a6209a5c46a2dd27ac225070
GoranAviani <goranaviani@gmail.com>
2021-02-21 15:35:59 +0100

    index page

--- doto_app_tryout/views.py
+++ doto_app_tryout/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render, redirect
 
 def index(request):
-    return redirect('dashboard') if request.user.is_authenticated else render(request, 'index.html')
+    return render(request, 'index.html')
