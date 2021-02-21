55def8ff6ae6b70c5d28285e8ae3463a9edfefee
GoranAviani <goranaviani@gmail.com>
2021-02-21 15:47:27 +0100

    todo lists listing empty lists

--- todo/urls.py
+++ todo/urls.py
@@ -7,4 +7,12 @@ app_name = "todo"
 
 urlpatterns = [
     path("", views.list_lists, name="lists"),
+    path("mine/", views.list_detail, {"list_slug": "mine"}, name="mine"),
+    path(
+        "<int:list_id>/<str:list_slug>/completed/",
+        views.list_detail,
+        {"view_completed": True},
+        name="list_detail_completed",
+    ),
+    path("<int:list_id>/<str:list_slug>/", views.list_detail, name="list_detail"),
     ]
